0b08c493b9802c9eb67be3ec607f8ad31389a6a7
Felipe Pinto <felipe14062000@gmail.com>
2022-08-31 12:51:02 -0300

    Resolução do primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/album/:id"
+              render={ (props) => <Album { ...props } /> }
+            />
+            <Route
+              exact
+              path="/profile/edit"
+              render={ (props) => <ProfileEdit { ...props } /> }
+            />
+            <Route
+              exact
+              path="/favorites"
+              render={ (props) => <Favorites { ...props } /> }
+            />
+            <Route
+              exact
+              path="/profile"
+              render={ (props) => <Profile { ...props } /> }
+            />
+            <Route
+              exact
+              path="/search"
+              render={ (props) => <Search { ...props } /> }
+            />
+            <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+            <Route
+              exact
+              path="/:any/:anysubpath/:anysubsubpath"
+              render={ (props) => <NotFound { ...props } /> }
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        Não temos nada aqui, verifique a URL usada!
+      </div>
+    );
+  }
+}
+
+export default NotFound;
